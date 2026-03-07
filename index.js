8da5593003fffd78db2dd5a6bfcc5abc4eaeca24
tanvir hasan joy <tanvirhasanjoy18@gmail.com>
2026-03-08 03:04:37 +0600

    solve the cors deploy issue

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 // =========================
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173", 
+      "https://pro-fast-87db5.web.app" // Add your production URL here
+    ],
     credentials: true,
   })
 );
